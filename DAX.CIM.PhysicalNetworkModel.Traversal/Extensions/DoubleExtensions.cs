5854ec64a88cd5b93cf255fe05166d07bec573e1
Mogens Heller Grabe <mookid8000@gmail.com>
2017-02-03 11:04:16 +0100

    experiment

---
+++ DAX.CIM.PhysicalNetworkModel.Traversal/Extensions/DoubleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DAX.CIM.PhysicalNetworkModel.Traversal.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static bool IsEqualTo(this double value, double other, double? tolerance = null)
+        {
+            var currentTolerance = tolerance ?? CimContext.GetCurrent().Tolerance;
+
+            return Math.Abs(value - other) < currentTolerance;
+        }
+    }
+}
---
+++ DAX.CIM.PhysicalNetworkModel.Traversal/Internals/InMemCimContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DAX.CIM.PhysicalNetworkModel.Traversal.Internals
+{
+    class InMemCimContext : CimContext
+    {
+        readonly Dictionary<string, IdentifiedObject> _objects = new Dictionary<string, IdentifiedObject>();
+        readonly Dictionary<string, List<IdentifiedObject>> _duplicates = new Dictionary<string, List<IdentifiedObject>>();
+
+        public InMemCimContext(IEnumerable<IdentifiedObject> objects)
+        {
+            Load(objects);
+        }
+
+        public override int Count => _objects.Count;
+
+        void Load(IEnumerable<IdentifiedObject> objects)
+        {
+            foreach (var obj in objects)
+            {
+                var id = obj.mRID;
+
+                if (_objects.ContainsKey(id))
+                {
+                    List<IdentifiedObject> duplicates;
+
+                    if (!_duplicates.TryGetValue(id, out duplicates))
+                    {
+                        duplicates = new List<IdentifiedObject> { _objects[id] };
+                        _duplicates[id] = duplicates;
+                    }
+
+                    duplicates.Add(obj);
+                }
+                else
+                {
+                    _objects[id] = obj;
+                }
+            }
+        }
+
+        public override IEnumerable<TIdentifiedObject> OfType<TIdentifiedObject>()
+        {
+            return _objects.Values.OfType<TIdentifiedObject>();
+        }
+
+        public override TIdentifiedObject GetObject<TIdentifiedObject>(string mRID)
+        {
+            IdentifiedObject obj;
+
+            if (!_objects.TryGetValue(mRID, out obj))
+            {
+                throw new ArgumentException($"Could not find object with mRID {mRID}");
+            }
+
+            try
+            {
+                return (TIdentifiedObject) obj;
+            }
+            catch (Exception exception)
+            {
+                throw new ArgumentException($"Could not return object with mRID {mRID} as {typeof(TIdentifiedObject)} because it is {obj.GetType()}", exception);
+            }
+        }
+
+        public override double Tolerance => 0.00001;
+    }
+}
