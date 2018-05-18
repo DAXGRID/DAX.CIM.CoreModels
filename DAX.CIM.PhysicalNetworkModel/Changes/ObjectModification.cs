bd8c505652aa624538c669f5fd9c61be94ccfa6f
Mogens Heller Grabe <mookid8000@gmail.com>
2018-05-18 13:38:17 +0200

    make object modifications into dictionaries

--- DAX.CIM.PhysicalNetworkModel/Changes/ObjectModification.cs
+++ DAX.CIM.PhysicalNetworkModel/Changes/ObjectModification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace DAX.CIM.PhysicalNetworkModel.Changes
@@ -7,6 +8,6 @@ namespace DAX.CIM.PhysicalNetworkModel.Changes
     [XmlType(Namespace = "http://daxgrid.net/PhysicalNetworkModel_0_1")]
     public class ObjectModification : ChangeSetMember
     {
-        public IdentifiedObject Object { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
     }
 }
