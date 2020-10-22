290c2a8278a5dac285c4af5db2561f7589f6a6bf
Jesper Ladegaard <jesper@dax.dk>
2020-10-22 09:23:42 +0200

    ShuntCompensatorInfoExt added

---
+++ DAX.CIM.PhysicalNetworkModel/Extensions/ShuntCompensatorInfoExt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAX.CIM.PhysicalNetworkModel
+{
+    public partial class ShuntCompensatorInfoExt : AssetInfo
+    {
+        private Voltage ratedVoltageField;
+
+        private ReactivePower maximumReactivePowerField;
+
+        private ReactivePower minimumReactivePowerField;
+
+        private ReactivePower actualReactivePowerField;
+
+        private ActivePower lossField;
+
+        private float qualityFactoryField;
+
+        private string technologyField;
+
+
+        public Voltage ratedVoltage => ratedVoltageField;
+      
+        public ReactivePower minimumReactivePower => minimumReactivePowerField;
+
+        public ReactivePower maximumReactivePower => maximumReactivePower;
+
+        public ReactivePower actualReactivePower => actualReactivePowerField;
+
+        public ActivePower loss => lossField;
+
+        public float qualityFactor => qualityFactoryField;
+
+        public string technology => technologyField;
+
+    }
+
+}
